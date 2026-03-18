17277acb9277f5dc27ed9a881d7dce3aa2b07708
lhaki2004 <lhakiscoutyangden2004@gmail.com>
2026-03-18 05:08:55 +0000

    User creation and Role creation done

---
+++ src/main/java/bt/edu/gcit/usermicroservice/dao/RoleDAOImpl.java
@@ -0,0 +1,25 @@
+package bt.edu.gcit.usermicroservice.dao;
+
+import bt.edu.gcit.usermicroservice.entity.Role;
+import org.springframework.stereotype.Repository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository
+public class RoleDAOImpl implements RoleDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    public RoleDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void addRole(Role role) {
+        // TODO Auto-generated method
+        entityManager.persist(role);
+    }
+}
---
+++ src/main/java/bt/edu/gcit/usermicroservice/dao/UserDAOImpl.java
@@ -0,0 +1,24 @@
+package bt.edu.gcit.usermicroservice.dao;
+
+import bt.edu.gcit.usermicroservice.entity.User;
+import org.springframework.stereotype.Repository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public User save(User user) {
+        return entityManager.merge(user);
+    }
+}
